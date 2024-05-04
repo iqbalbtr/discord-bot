b41a6741f565595eeeb060e05559dbe3176e5c91
Iqbal Bahtiar <iqbalbahtiar045@gmail.com>
2024-05-05 00:44:04 +0700

    initial

---
+++ src/commands/commands.js
@@ -0,0 +1,44 @@
+import 'dotenv/config';
+import { getRPSChoices } from "./utility/game.js"
+import { capitalize } from "./utils.js"
+
+// Get the game choices from game.js
+function createCommandChoices() {
+  const choices = getRPSChoices();
+  const commandChoices = [];
+
+  for (let choice of choices) {
+    commandChoices.push({
+      name: capitalize(choice),
+      value: choice.toLowerCase(),
+    });
+  }
+
+  return commandChoices;
+}
+
+// Simple test command
+const TEST_COMMAND = {
+  name: 'test',
+  description: 'Basic command',
+  type: 1,
+};
+
+// Command containing options
+const CHALLENGE_COMMAND = {
+  name: 'challenge',
+  description: 'Challenge to a match of rock paper scissors',
+  options: [
+    {
+      type: 3,
+      name: 'object',
+      description: 'Pick your object',
+      required: true,
+      choices: createCommandChoices(),
+    },
+  ],
+  type: 1,
+};
+
+export const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+
